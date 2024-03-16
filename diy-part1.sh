1c285b0ff0836b802b38eeaec3115f508e183871
linhahaz <49563001+linhahaz@users.noreply.github.com>
2024-03-16 19:40:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,15 @@
 # See /LICENSE for more information.
 #
 
+# mv -f neihe/Makefile ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
+# sed -i 's/KERNEL_TESTING_PATCHVER:=6.6/KERNEL_TESTING_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+echo sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
